33041738a453abcfe0054c46d5055a5eb281fd77
黃琮淵 <NickHuang@huangcongyuandeMacBook-Pro.local>
2014-09-18 12:19:25 +0800

    Initial Commit

---
+++ MBook - Just Accounting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MBook - Just Accounting
+//
+//  Created by 黃琮淵 on 2014/9/18.
+//  Copyright (c) 2014年 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBookAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBookAppDelegate class]));
+    }
+}
